fd18e9b43872781505cc164f13e169f89161b79a
Phil Mansfield <mansfield@uchicago.edu>
2016-04-24 21:33:04 -0500

    Wrote compiling toy version of print_header.go

--- util/print_header.go
+++ util/print_header.go
@@ -25,9 +25,11 @@ func FilesetOpen(prefix string, flag int) (Fileset, error)  {
 	cStr := C.CString(prefix)
 	defer C.free(unsafe.Pointer(cStr))
 
-	fileset := C.artio_fileset_open(cStr, C.int(flag), (*C.artio_context)(nil))
+	fileset := Fileset{
+		C.artio_fileset_open(cStr, C.int(flag), (*C.artio_context)(nil)),
+	}
 
-	if fileset.ptr != (*C.artio_fileset)(nil) {
+	if fileset.ptr == (*C.artio_fileset)(nil) {
 		return fileset, fmt.Errorf("Prefix %s does not exist", prefix)
 	} else {
 		return fileset, nil
@@ -36,11 +38,9 @@ func FilesetOpen(prefix string, flag int) (Fileset, error)  {
 
 func PrintHeader(prefix string) {
 	_, err := FilesetOpen(prefix, 0)
-	if err != nil {
-		fmt.Println("File does not exist.")
-	} else {
-		fmt.Println("File exists.")
-	}
+
+	if err != nil { fmt.Println(err.Error()) }
+	fmt.Println("File exists.")
 }
 
 func main() {
