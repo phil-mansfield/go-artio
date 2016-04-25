86aa521072206d5087bc2ad434e1973d2c5fe41f
Phil Mansfield <mansfield@uchicago.edu>
2016-04-25 11:17:12 -0500

    particle_species_num added to print_first_n

--- util/print_first_n.go
+++ util/print_first_n.go
@@ -13,8 +13,17 @@ func PrintFirstN(prefix string, n int) error {
 	h, err := artio.FilesetOpen(prefix, 0, artio.NullContext)
 	if err != nil { return err }
 	defer h.Close()
-	
-	fmt.Println(h.GetInt(h.Key("num_particle_species")))
+
+	numSpecies := h.GetInt(h.Key("num_particle_species"))[0]
+	var counts []int64
+	if key := h.Key("particle_species_num"); key.Type == artio.Int {
+		counts32 := h.GetInt(key)
+		counts := make([]int64, len(counts32))
+		for i := range counts { counts[i] = int64(counts32[i]) }
+	} else {
+		counts = h.GetLong(key)
+	}
+	fmt.Println(numSpecies, counts)
 
 	return nil
 }
