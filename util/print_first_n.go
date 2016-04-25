97faca6fb9185578e30fef4b32969deb09617400
Phil Mansfield <mansfield@uchicago.edu>
2016-04-25 11:26:42 -0500

    Adapted PrintFirstN to num_particles_per_species

--- util/print_first_n.go
+++ util/print_first_n.go
@@ -9,21 +9,31 @@ import (
 	artio "github.com/phil-mansfield/go-artio"
 )
 
+
+func getIntOrLong(h artio.Fileset, key artio.Key) []int64 {
+	if key.Type == artio.Int {
+		out32 := h.GetInt(key)
+		out := make([]int64, len(out32))
+		for i := range out { out[i] = int64(out32[i]) }
+		return out
+	} else {
+		return h.GetLong(key)
+	}
+}
+
 func PrintFirstN(prefix string, n int) error {
 	h, err := artio.FilesetOpen(prefix, 0, artio.NullContext)
 	if err != nil { return err }
 	defer h.Close()
 
-	numSpecies := h.GetInt(h.Key("num_particle_species"))[0]
-	var counts []int64
-	if key := h.Key("particle_species_num"); key.Type == artio.Int {
-		counts32 := h.GetInt(key)
-		counts := make([]int64, len(counts32))
-		for i := range counts { counts[i] = int64(counts32[i]) }
+	var speciesNumKey artio.Key
+	if h.HasKey("particle_species_num") {
+		speciesNumKey = h.Key("particle_species_num")
 	} else {
-		counts = h.GetLong(key)
+		speciesNumKey = h.Key("num_particles_per_species")
 	}
-	fmt.Println(numSpecies, counts)
+	counts := getIntOrLong(h, speciesNumKey)
+	fmt.Println(counts)
 
 	return nil
 }
