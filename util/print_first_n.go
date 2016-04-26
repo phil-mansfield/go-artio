211af34ad552eeba80b75d544364308c998fa5bc
Phil Mansfield <mansfield@uchicago.edu>
2016-04-26 11:03:19 -0500

    Rewrote printf_first_n

--- util/print_first_n.go
+++ util/print_first_n.go
@@ -36,26 +36,40 @@ func PrintFirstN(prefix string, n int) error {
 	fileIndices := getIntOrLong(h, h.Key("particle_file_sfc_index"))
 	masses := h.GetFloat(h.Key("particle_species_mass"))
 
-	speciesIndices := make([]int64, len(counts) + 1)
-	for i := range counts {
-		speciesIndices[i+1] = speciesIndices[i] + counts[i]
-	}
-
 	err = h.OpenParticles()
 	if err != nil { return err }
 	defer h.CloseParticles()
 
-	err = h.ParticleCacheSfcRange(fileIndices[0], fileIndices[1] - 1)
+	err = h.ParticleCacheSfcRange(0, fileIndices[len(fileIndices) - 1] - 1)
 	if err != nil { return err }
 	defer h.ParticleClearSfcCache()
 
-	primary := make([]float64, h.GetInt(h.Key("num_primary_variables"))[0])
-	secondary := make([]float32, h.GetInt(h.Key("num_secondary_variables"))[0])
-	id, subspecies, err := h.ReadParticle(primary, secondary)
-	if err != nil { return err }
-	fmt.Println(id, subspecies)
-	fmt.Println(primary)
-	fmt.Println(counts, masses)
+	roots := h.GetLong(h.Key("num_root_cells"))[0]
+	numSpeciesBuf := make([]int, len(counts))
+	numParticlesRead := 0
+
+	primaryBuf := make([]float64, h.GetInt(h.Key("num_primary_variables"))[0])
+	secondaryBuf := make([]float32,
+		h.GetInt(h.Key("num_secondary_variables"))[0],
+	)
+
+RootLoop:
+	for root := int64(0); root < roots; root++ {
+		h.ParticleReadRootCellBegin(root, numSpeciesBuf)
+
+		h.ParticleReadSpeciesBegin(0)
+		for i := 0; i < numSpeciesBuf[0]; i++ {
+			h.ReadParticle(primaryBuf, secondaryBuf)
+			fmt.Printf("%.3g\n", primaryBuf)
+			numParticlesRead++
+			if numParticlesRead == n { break RootLoop }
+		}
+		h.ParticleReadSpeciesEnd()
+
+		h.ParticleReadRootCellEnd()
+	}
+
+	fmt.Println(masses)
 
 	return nil
 }
