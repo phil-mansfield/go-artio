f25223746d4de7ef004c085e57e6e2ffca92529d
Phil Mansfield <mansfield@uchicago.edu>
2016-04-26 15:36:23 -0500

    Added CountCallback function to inlined C.

--- artio.go
+++ artio.go
@@ -7,7 +7,19 @@ package artio
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
+#include <stdint.h>
 #include "artio.h"
+
+// `count_callback` counts the number of particles of each type in a range.
+// `params` is a zeroed int64_t array that output will be written to.
+static inline int CountCallback(
+	int64_t sfc_index, int species, int subspecies, int64_t pid,
+	double *primary_variables, float *secondary_variables, void *params
+) {
+	int64_t *counts = (int64_t *) params;
+	counts[species]++;
+	return 0;
+}
 */
 import "C"
 
